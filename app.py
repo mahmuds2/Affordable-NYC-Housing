15241aa43a045049011b11798b7c69461dc7c60a
Shazmin Mahmud <sm4423@columbia.edu>
2019-03-29 16:48:29 -0400

    Building method to get NTA code of NYCHA buildings

--- app.py
+++ app.py
@@ -1,23 +1,30 @@
 from flask import Flask, render_template
 from sodapy import Socrata
+import googlemaps
 import pandas as pd
+import requests
 import os
 
 app = Flask(__name__)
 client = Socrata("data.cityofnewyork.us", None)
 gmaps_token = os.environ['GOOGLE_MAPS_TOKEN']
+gmaps = googlemaps.Client(key=gmaps_token)
 
 @app.route('/')
 def map():
-    # First 2000 results, returned as JSON from API / converted to Python list of
-    # dictionaries by sodapy.
-    results = client.get("q2z5-ai38", limit=200)
+    # get information on all ntas and nycha buildings
+    ntas = client.get("q2z5-ai38")
+    nycha = client.get("evjd-dqpz", limit=2,
+        select="location_street_a, location_street_b, location_street_c, location_street_d, borough")
+
+    print("nycha:" + str(nycha))
+    get_building_nta(nycha)
 
     # Convert to pandas DataFrame
-    results_df = pd.DataFrame.from_records(results)
+    ntas_df = pd.DataFrame.from_records(ntas)
 
-    polygons = draw_neighborhoods(results_df)
-    neighborhoods = get_neighborhood_info(results_df)
+    polygons = draw_neighborhoods(ntas_df)
+    neighborhoods = get_neighborhood_info(ntas_df)
 
     return render_template('index.html', polygons=polygons,
                                         neighborhoods=neighborhoods,
@@ -25,12 +32,15 @@ def map():
 
 def draw_neighborhoods(results):
     '''
-    Return: Dictionary where key is nta name and value is array of coordinate path of neighborhood
+    Parameter Type: Pandas DataFrame
+    Information of all ntas in NYC
+
+    Return: Dictionary
+    Coordinates of all ntas, where key is name and value is array of nta's coordinate path
     '''
 
     polygons = {}
 
-    print(results.columns)
     for neighborhood in results.iterrows():
         polygons[neighborhood[1]['ntaname']] = get_polygon(neighborhood[1]['the_geom']['coordinates'])
 
@@ -38,7 +48,11 @@ def draw_neighborhoods(results):
 
 def get_neighborhood_info(results):
     '''
-    Return: Dictionary where key is nta name and values are nta codes
+    Parameter Type: Pandas DataFrame
+    Set of information of all ntas in NYC
+
+    Return Type: Dictionary
+    All nta names and values, where key is name and values are codes
     '''
     neighborhoods = {}
 
@@ -48,9 +62,30 @@ def get_neighborhood_info(results):
     return neighborhoods
 
 def get_polygon(neighborhood_coords):
+    '''
+    Parameter Type: Column of Pandas Dictionary
+    All nta coordinates in NYC in format [longitude, latitude]
+
+    Return Type: Array
+    Array of tuples containing coordinate path of ntas in format [latitude, longitude]
+    '''
     path = []
 
     for coord in neighborhood_coords[0][0]:
         path.append([coord[0], coord[1]])
 
     return path
+
+def get_building_nta(address):
+    '''
+    Parameter Types: String
+    Building address
+
+    Return: String
+    NTA code of building
+    '''
+    # get latitude and longtitude of Building
+    geocode_result = gmaps.geocode(address)
+
+    for building_info in geocode_result:
+        print(str(building_info['formatted_address']), str(building_info['geometry']['location']) + '\n')
